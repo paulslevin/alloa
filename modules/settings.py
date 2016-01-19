e6122be4ac6957f7cb13aad8f365267b4bc1b4d1
paulslevin <paul.slevin@cantab.net>
2016-01-19 16:48:20 +0000

    Fix the settings os.path stuff
    As above, but alloa.py still won't run properly from the command line.

--- modules/settings.py
+++ modules/settings.py
@@ -6,9 +6,13 @@ import os
 import time
 
 DATE = time.strftime("%d%m%y")
+CURRENT = os.path.dirname(os.path.realpath(__file__))
 
 config = ConfigParser.RawConfigParser()
-config.read(os.path.join(os.path.realpath(os.getcwd()), "alloa.conf"))
+config.read(os.path.join(CURRENT, "alloa.conf"))
+
+
+print CURRENT
 
 ALLOCATION_PROFILE_FILENAME = "allocation_profile_" + DATE + ".csv"
 ALLOCATION_FILENAME = "allocation_" + DATE + ".csv"
@@ -19,26 +23,26 @@ ALLOCATION_FILENAME = "allocation_" + DATE + ".csv"
 # Path for storing temporary files
 
 WORKING_FILES = config.get("temporary_files", "working_files").replace("'", "")
-WORKING_PATH = os.path.abspath(os.path.join(os.path.realpath(__file__),
+WORKING_PATH = os.path.abspath(os.path.join(CURRENT,
                                             os.pardir,
                                             os.path.normpath(WORKING_FILES)))
 
 ALLOCATION_PROFILE_PATH = os.path.abspath(
-    os.path.join(os.path.realpath(os.getcwd()),
+    os.path.join(CURRENT,
                  os.pardir,
                  WORKING_FILES,
                  ALLOCATION_PROFILE_FILENAME)
 )
 
 ALLOCATION_PATH = os.path.abspath(
-    os.path.join(os.path.realpath(os.getcwd()),
+    os.path.join(CURRENT,
                  os.pardir,
                  WORKING_FILES,
                  ALLOCATION_FILENAME)
 )
 
 LEVEL1_DATA = config.get("main_allocation_data", "level1_data").replace("'", "")
-LEVEL1_PATH = os.path.abspath(os.path.join(os.path.realpath(os.getcwd()),
+LEVEL1_PATH = os.path.abspath(os.path.join(CURRENT,
                                            os.pardir,
                                            WORKING_FILES,
                                            LEVEL1_DATA))
@@ -48,7 +52,7 @@ LEVEL1_DELIMITER = config.get("main_allocation_data",
 LEVEL1_NAME = config.get("output_files", "level1").replace("'", "")
 
 LEVEL2_DATA = config.get("main_allocation_data", "level2_data").replace("'", "")
-LEVEL2_PATH = os.path.abspath(os.path.join(os.path.realpath(os.getcwd()),
+LEVEL2_PATH = os.path.abspath(os.path.join(CURRENT,
                                            os.pardir,
                                            WORKING_FILES,
                                            LEVEL2_DATA))
@@ -57,7 +61,7 @@ LEVEL2_DELIMITER = config.get("main_allocation_data",
 LEVEL2_NAME = config.get("output_files", "level2").replace("'", "")
 
 LEVEL3_DATA = config.get("main_allocation_data", "level3_data").replace("'", "")
-LEVEL3_PATH = os.path.abspath(os.path.join(os.path.realpath(os.getcwd()),
+LEVEL3_PATH = os.path.abspath(os.path.join(CURRENT,
                                            os.pardir,
                                            WORKING_FILES,
                                            LEVEL3_DATA))
@@ -70,7 +74,7 @@ LEVEL4_ALLOCATION = config.get("second_supervisor_allocation",
 
 LEVEL4_DATA = config.get("second_supervisor_allocation",
                          "level4_data").replace("'", "")
-LEVEL4_PATH = os.path.abspath(os.path.join(os.path.realpath(os.getcwd()),
+LEVEL4_PATH = os.path.abspath(os.path.join(CURRENT,
                                            os.pardir,
                                            WORKING_FILES,
                                            LEVEL4_DATA))
