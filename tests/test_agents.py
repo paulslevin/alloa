be45ac9d8fbba39a091d48fe0803b4b918fb31b0
Paul Slevin <pt.slevin@gmail.com>
2017-09-25 18:17:16 +0100

    Add unit test for hierarchy preferred method.

--- tests/test_agents.py
+++ tests/test_agents.py
@@ -1,3 +1,4 @@
+from itertools import izip
 import unittest
 from modules.agents import Agent, Hierarchy
 
@@ -41,3 +42,42 @@ class TestHierarchy(unittest.TestCase):
         agent1 = Agent(id_number=1, hierarchy=self.hierarchy)
         self.hierarchy.add_agent(agent1)
         self.assertEqual(self.hierarchy.number_of_agents, 1)
+
+    def test_preferred(self):
+
+        # Level 1 agents.
+        agent_1_1 = Agent(id_number=1, hierarchy=self.hierarchy)
+        agent_1_2 = Agent(id_number=2, hierarchy=self.hierarchy)
+        agent_1_3 = Agent(id_number=3, hierarchy=self.hierarchy)
+
+        # Level 2 agents.
+        hierarchy2 = Hierarchy(level=2)
+        agent_2_4 = Agent(id_number=4, hierarchy=hierarchy2)
+        agent_2_5 = Agent(id_number=5, hierarchy=hierarchy2)
+        agent_2_6 = Agent(id_number=6, hierarchy=hierarchy2)
+        agent_2_7 = Agent(id_number=7, hierarchy=hierarchy2)
+        agent_2_8 = Agent(id_number=8, hierarchy=hierarchy2)
+
+        agent_1_1.preferences = [agent_2_4, agent_2_5, agent_2_6]
+        agent_1_2.preferences = [agent_2_5, agent_2_6, agent_2_7]
+        agent_1_3.preferences = [agent_2_8]
+
+        test_cases = [
+
+            # Size 1 subsets.
+            ({agent_1_1}, [agent_2_4, agent_2_5, agent_2_6]),
+            ({agent_1_2}, [agent_2_5, agent_2_6, agent_2_7]),
+            ({agent_1_3}, [agent_2_8]),
+
+            # Size 2 subsets.
+            ({agent_1_1, agent_1_2}, [agent_2_4, agent_2_5, agent_2_6, agent_2_7]),
+            ({agent_1_1, agent_1_3}, [agent_2_4, agent_2_5, agent_2_6, agent_2_8]),
+            ({agent_1_2, agent_1_3}, [agent_2_5, agent_2_6, agent_2_7, agent_2_8]),
+
+            # Size 3 subset.
+            ({agent_1_1, agent_1_2, agent_1_3}, 
+             [agent_2_4, agent_2_5, agent_2_6, agent_2_7, agent_2_8]),
+        ]
+
+        for agent_subset, expected in test_cases:
+            self.assertEqual(self.hierarchy.preferred(agent_subset), expected)
